1bc766bb006f42f7abfb0b1784070b0941e7fe7f
snikolaj <51296436+snikolaj@users.noreply.github.com>
2022-09-05 13:34:19 +0200

    Update firstsketch.java

--- firstsketch.java
+++ firstsketch.java
@@ -4,6 +4,8 @@ float a = 0;
 float x_start = 200;
 float y_start = 200;
 
+final float maxDist = 300;
+
 class Rectangle{
   float cx;
   float cy;
@@ -11,7 +13,21 @@ class Rectangle{
   float w;
 }
 
-int rectNum = 1;
+class Point{
+  float px;
+  float py;
+}
+
+float euclideanDist(float x, float y, Point p){
+  return sqrt((x - p.px)*(x - p.px) + (y - p.py)*(y - p.py));
+}
+
+float euclideanDist(float x, float y, float px, float py){
+  return sqrt((x - px)*(x - px) + (y - py)*(y - py));
+}
+
+
+int rectNum = 2;
 
 Rectangle rects[]= new Rectangle[rectNum];
 
@@ -27,10 +43,10 @@ void setup(){
   rects[0].h = 100;
   rects[0].w = 100;
   
-  /*rects[1].cx = 250;
-  rects[1].cy = 250;
+  rects[1].cx = 200;
+  rects[1].cy = 150;
   rects[1].h = 50;
-  rects[1].w = 50;*/
+  rects[1].w = 50;
   
 }
 
@@ -39,21 +55,27 @@ void draw(){
   
   for(int i = 0; i < rectNum; i++){
     rect(rects[i].cx, rects[i].cy, rects[i].h, rects[i].w);
-    lineIntersects(x_start, y_start, x_start + 100 * cos(a + r), y_start + 100 * sin(a + r), rects[i]);
   }
   
-  line(x_start, y_start, x_start + 100 * cos(a + r), y_start + 100 * sin(a + r));
-  
-  
-  /*for(a = 0; a < PI / 2; a += (PI / 2) / 90){
-    line(x_start, y_start, x_start + 100 * cos(a + r), y_start + 100 * sin(a + r));
+  for(a = 0; a < PI / 2; a += (PI / 2) / 180){
+    float dist = maxDist;
     
-  }*/
+    
+    for(int i = 0; i < rectNum; i++){
+      float tempDist = lineIntersects(x_start, y_start, x_start + maxDist * cos(a + r), y_start + maxDist * sin(a + r), rects[i]);
+      if(dist > tempDist){
+        dist = tempDist;
+      }
+    }
+    
+    line(x_start, y_start, x_start + dist * cos(a + r), y_start + dist * sin(a + r));
+  }
 }
 
-boolean lineIntersects(float x1, float y1, float x2, float y2, Rectangle rectcheck){
+float lineIntersects(float x1, float y1, float x2, float y2, Rectangle rectcheck){
   float t, u;
   float px, py;
+  float dist = maxDist;
   
   // upper line
   t = ((y1 - rectcheck.cy)*(rectcheck.w))/((y1 - y2)*(rectcheck.w));
@@ -62,7 +84,9 @@ boolean lineIntersects(float x1, float y1, float x2, float y2, Rectangle rectche
   if(0 <= t && t <= 1 && 0 <= u && u <= 1){
     px = (x1 + t*(x2 - x1));
     py = (y1 + t*(y2 - y1));
-    circle(px, py, 5);
+    if(euclideanDist(x1, y1, px, py) < dist){
+      dist = euclideanDist(x1, y1, px, py);
+    }  
   }
 
   // left line
@@ -72,7 +96,9 @@ boolean lineIntersects(float x1, float y1, float x2, float y2, Rectangle rectche
   if(0 <= t && t <= 1 && 0 <= u && u <= 1){
     px = (x1 + t*(x2 - x1));
     py = (y1 + t*(y2 - y1));
-    circle(px, py, 5);
+    if(euclideanDist(x1, y1, px, py) < dist){
+      dist = euclideanDist(x1, y1, px, py);
+    }  
   }
   
   // bottom line
@@ -82,7 +108,9 @@ boolean lineIntersects(float x1, float y1, float x2, float y2, Rectangle rectche
   if(0 <= t && t <= 1 && 0 <= u && u <= 1){
     px = (x1 + t*(x2 - x1));
     py = (y1 + t*(y2 - y1));
-    circle(px, py, 5);
+    if(euclideanDist(x1, y1, px, py) < dist){
+      dist = euclideanDist(x1, y1, px, py);
+    }  
   }
   
   // right line
@@ -92,10 +120,12 @@ boolean lineIntersects(float x1, float y1, float x2, float y2, Rectangle rectche
   if(0 <= t && t <= 1 && 0 <= u && u <= 1){
     px = (x1 + t*(x2 - x1));
     py = (y1 + t*(y2 - y1));
-    circle(px, py, 5);
+    if(euclideanDist(x1, y1, px, py) < dist){
+      dist = euclideanDist(x1, y1, px, py);
+    }  
   }
   
-  return false;
+  return dist;
 }
 
 
